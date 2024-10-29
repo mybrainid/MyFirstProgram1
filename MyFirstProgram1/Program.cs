34354f8f1864e157dc6237b03164dba083c14745
Srinivasan S <36277117+mybrainid@users.noreply.github.com>
2024-10-29 13:16:12 +0530

    Update Program.cs
    Keep one commit messages.

--- MyFirstProgram1/Program.cs
+++ MyFirstProgram1/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# program!, One more modificaction.");
-Console.WriteLine("Hello, C# program!, One more modificaction.");
-Console.WriteLine("Hello, C# program!, One more modificaction.");
-Console.WriteLine("Hello, C# program!, One more modificaction.");
-Console.WriteLine("Hello, C# program!, One more modificaction.");
+
 Console.ReadLine();
