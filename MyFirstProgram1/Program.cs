246ca3aa5fc6463ea0c9afb18a59529d9c64b32b
shankarnsrini <115606526+shankarnsrini@users.noreply.github.com>
2024-10-31 14:04:14 +0530

    Started the math game.

--- MyFirstProgram1/Program.cs
+++ MyFirstProgram1/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# program!, One more modificaction.");
 
-Console.ReadLine();
+Console.WriteLine("Please type your name:");
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself");
+Console.WriteLine($@"What would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("----------------------------------------");
+
