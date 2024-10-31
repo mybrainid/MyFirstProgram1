3bb8c9cc9f8669fa9185f2b87106b8e3413b6b7b
shankarnsrini <115606526+shankarnsrini@users.noreply.github.com>
2024-10-31 17:15:02 +0530

    Implemented trim and toupper methods for input

--- MyFirstProgram1/Program.cs
+++ MyFirstProgram1/Program.cs
@@ -5,12 +5,43 @@ var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
 Console.WriteLine("----------------------------------------");
-Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself");
 Console.WriteLine($@"What would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
 M - Multiplication
 D - Division
 Q - Quit the program");
-Console.WriteLine("----------------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+if(gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("addition game selected");
+
+} else if(gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("subtraction game selected");
+
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("multiplication game selected");
+
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("division game selected");
+
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("invalid input");
+}
+
 
